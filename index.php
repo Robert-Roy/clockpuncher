b1778d769af10d4cebbb9b68a18d0395a0326fbe
Robert-Roy <robertproy@live.com>
2017-01-16 22:42:22 -0600

    create index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+All rights reserved. Copyright Robert Roy 2016.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
