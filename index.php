7385b3675ab3e48300945b1ecc14fe7f3372afdd
QuinnWhitaker <qmwhitaker@gmail.com>
2017-01-18 15:58:19 -0800

    change appearance

--- index.php
+++ index.php
@@ -7,15 +7,16 @@ include_once('header.php')
 ?>
 
 <div class="contentdiv">
-    <h2>Please sign in</h2>
+    <h2>Clock Puncher</h2>
+    <br>
     <form action="sendmail.php" method="post" onsubmit="return false;">
-        <label class="label" for="email">Email:</label>
-        <input type="text" name="email" autocomplete="email" placeholder="johndoe@gmail.com">
+        <!--label class="label" for="email">Email:</label>-->
+        <center><input type="text" name="email" autocomplete="email" placeholder="email address">
+        <!--label class="label"  for="pw">Password:</label>-->
+        <input type="text" name="pw" placeholder="password"></center>
         <br>
-        <label class="label"  for="pw">Password:</label>
-        <input type="text" name="pw" placeholder="Password123">
-        <br>
-        <input class="crispbutton" type="submit" value="Clock In">
+        <input class="crispbutton" type="submit" value="Sign In">
+        
     </form>
 </div>
 
