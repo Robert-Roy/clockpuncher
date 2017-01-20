9511016ac9a1874b191ca91f40c0afcaac10afb7
Robert-Roy <robertproy@live.com>
2017-01-19 18:46:23 -0600

    add sqlconnectorexample

---
+++ sql/sqlconnectorexample.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+/*
+ * All rights reserved. Copyright Robert Roy 2016.
+ */
+class SQLConnector {
+    private $conn;
+    public function getConn(){
+        return $this->conn;
+    }
+    public function __construct() {
+        $this->conn = $this->Conn();
+    }
+    public static function Conn() {
+        try {
+            $servername = "localhost";
+            $username = "XXXXXXXXXXXX";
+            $porkchop = "XXXXXXXXXXXX";
+            $dbname = "XXXXXXXXXXXXXX";
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $porkchop);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+
+}
+?>
